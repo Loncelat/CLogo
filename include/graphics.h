57983183663f9e29cd7804abeaf581ab56fe31fa
Martijn Leus <martijnleus@live.nl>
2018-11-09 12:31:44 +0100

    Updated drawing code.

--- include/graphics.h
+++ include/graphics.h
@@ -9,9 +9,17 @@
 #define WIDTH  (400)
 #define HEIGHT (400)
 
+#define RENDERER_FLAGS (SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE)
+
 #define INIT_TURTLE_X (WIDTH / 2.0)
 #define INIT_TURTLE_Y (HEIGHT / 2.0)
 #define TURTLE_DIRECTIONS (8)
+
+#define FRAMES_PER_SECOND (60)
+#define FRAME_FREQUENCY (1000.0 / FRAMES_PER_SECOND)
+
+#define VSYNC_SHOULD_WAIT ((SDL_GetPerformanceCounter() - vsyncStartTime) * freqMultiplier < FRAME_FREQUENCY)
+#define VSYNC_WAIT (SDL_Delay((FRAME_FREQUENCY) - (SDL_GetPerformanceCounter() - vsyncStartTime) * freqMultiplier))
 
 typedef struct Turtle {
     double x;
@@ -26,10 +34,19 @@ typedef struct Turtle {
 
     SDL_Rect dstrect;
 
-} Turtle_t;
+} turtle_t;
+
+typedef struct Colour {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+} colour_t;
 
 int InitGraphics(void);
-int InitTurtle(Turtle_t *turtle);
-void ResetTurtle(Turtle_t *turtle);
+int InitTurtle(turtle_t *turtle);
+void ResetTurtle(turtle_t *turtle);
 
-void DrawTurtle(SDL_Renderer *renderer, Turtle_t *turtle);
+void ClearGraphics(SDL_Renderer *renderer);
+
+void Draw(void);
+void DrawTurtle(SDL_Renderer *renderer, turtle_t *turtle);
--- src/graphics.c
+++ src/graphics.c
@@ -1,50 +1,70 @@
 #include "graphics.h"
 
-Turtle_t turtle;
+turtle_t turtle;
+
+colour_t bgClr = {0, 255, 255};
+colour_t fgClr = {0, 0, 0};
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
+SDL_Texture *background = NULL;
 SDL_Texture *canvas = NULL;
+
+uint64_t vsyncStartTime = 0;
+double freqMultiplier = 0;
 
 int InitGraphics(void) {
     window = SDL_CreateWindow(
-            "CLogo",
-            SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED,
-            WIDTH,
-            HEIGHT,
-            0);
+        "CLogo",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        WIDTH,
+        HEIGHT,
+        0
+    );
 
     if (window == NULL) {
         printf("SDL2 Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    renderer = SDL_CreateRenderer(window, -1, RENDERER_FLAGS);
 
     if (renderer == NULL) {
         printf("SDL2 Error: %s\n", SDL_GetError());
         return 1;
     }
 
+    /* Create the textures. */
+    background = SDL_CreateTexture(        
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_TARGET,
+        WIDTH,
+        HEIGHT
+    );
+
     canvas = SDL_CreateTexture(
         renderer,
-        SDL_PIXELFORMAT_RGB24,
-        SDL_TEXTUREACCESS_STREAMING,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_TARGET,
         WIDTH,
-        HEIGHT);
+        HEIGHT
+    );
+
+    SDL_SetTextureBlendMode(canvas, SDL_BLENDMODE_BLEND);
 
     if (InitTurtle(&turtle)) {
         printf("SDL2 Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+    freqMultiplier = 1000.0 / SDL_GetPerformanceFrequency();
 
-    SDL_RenderClear(renderer);
-    DrawTurtle(renderer, &turtle);
-    
-    SDL_RenderPresent(renderer);
+    SDL_SetRenderTarget(renderer, canvas);
+    ClearGraphics(renderer);
+
+    Draw();
 
     return 0;
 }
@@ -66,7 +86,7 @@ SDL_Texture* LoadImage(char* file) {
     return texture;
 }
 
-int InitTurtle(Turtle_t *turtle) {
+int InitTurtle(turtle_t *turtle) {
 
     ResetTurtle(turtle);
     turtle->texture = LoadImage("graphics/turtle.png");
@@ -86,14 +106,57 @@ int InitTurtle(Turtle_t *turtle) {
     return 0;
 }
 
-void ResetTurtle(Turtle_t *turtle) {
+void ResetTurtle(turtle_t *turtle) {
     turtle->x = INIT_TURTLE_X;
     turtle->y = INIT_TURTLE_Y;
     turtle->angle = 0;
     turtle->rotation = 0;
 }
 
-void DrawTurtle(SDL_Renderer *renderer, Turtle_t *turtle) {
+void ClearGraphics(SDL_Renderer *renderer) {
+
+    /* Reset background colour to white */
+    bgClr = (colour_t) {255, 255, 255};
+    SDL_SetRenderDrawColor(renderer, 0, bgClr.g, bgClr.b, SDL_ALPHA_OPAQUE);
+
+    /* Clear the background */
+    SDL_SetRenderTarget(renderer, background);
+    SDL_RenderClear(renderer);
+
+    /* Clear the canvas (transparent) */
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_SetRenderTarget(renderer, canvas);
+    SDL_RenderClear(renderer);
+
+    /* Restore the colour to the foreground colour */
+    SDL_SetRenderDrawColor(renderer, fgClr.r, fgClr.g, fgClr.b, SDL_ALPHA_OPAQUE);
+
+}
+
+void Draw(void) {
+    if (VSYNC_SHOULD_WAIT) {
+        VSYNC_WAIT;
+    }
+
+    /* Set render target to the window. */
+    SDL_SetRenderTarget(renderer, NULL);
+
+    SDL_RenderClear(renderer);
+
+    /* Draw the background, canvas and turtle. */
+    SDL_RenderCopy(renderer, background, NULL, NULL);
+    SDL_RenderCopy(renderer, canvas, NULL, NULL);
+    DrawTurtle(renderer, &turtle);
+
+    SDL_RenderPresent(renderer);
+
+    /* Restore the render target to the canvas. */
+    SDL_SetRenderTarget(renderer, canvas);
+
+    vsyncStartTime = SDL_GetPerformanceCounter();
+}
+
+void DrawTurtle(SDL_Renderer *renderer, turtle_t *turtle) {
 
     SDL_Rect srcrect = (SDL_Rect) {
         turtle->width * turtle->rotation,
