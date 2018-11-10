79112f2b8d2467b9a4332f1abf22eb77eac6a843
Martijn Leus <martijnleus@live.nl>
2018-11-10 09:32:06 +0100

    Add preliminary argument support.

--- include/tokenizer.h
+++ include/tokenizer.h
@@ -4,7 +4,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <ctype.h>
 
+#include "types.h"
 #include "token.h"
 #include "commands.h"
 
@@ -12,4 +14,5 @@
 
 tokenlist_t Tokenize(char* str);
 size_t GetNextEndIndex(char* str, size_t start, size_t len);
+data_t* GetNextData(char* str, size_t* iptr, size_t len, uint32_t type);
 uint64_t GetHashCode(char* str);
--- src/tokenizer.c
+++ src/tokenizer.c
@@ -18,23 +18,40 @@ tokenlist_t Tokenize(char* str) {
         /* Don't create whitespace tokens */
         if (startIndex < endIndex) {
             /* Allocate a new token_t into the tokenList */
-            token_t *tok = calloc(1, sizeof(token_t));
+            token_t *token = calloc(1, sizeof(token_t));
 
             /* Copy the string into the created token. */
-            tok->value = calloc(endIndex - startIndex + 1, sizeof(char));
-            memcpy(tok->value, str + startIndex, endIndex - startIndex);
+            token->value = calloc(endIndex - startIndex + 1, sizeof(char));
+            memcpy(token->value, str + startIndex, endIndex - startIndex);
             
-            tok->hash = GetHashCode(tok->value);
+            token->hash = GetHashCode(token->value);
 
-            // TODO: determine arguments
+            int32_t argc = GetArgumentCount(token->hash);
+            if (argc == -1) {
+                token->argc = 0;
+            } else if (argc > 0) {
+                token->arg = calloc(argc, sizeof(data_t*));
+
+                //TODO: Support for text in data.
+                for (int32_t i = 0; i < argc; ++i) {
+                    token->arg[i] = GetNextData(str, &endIndex, len, DATA_NUM);
+
+                    if (token->arg[i] == NULL) {
+                        printf("%s misses %d argument(s)\n", token->value, argc - i);
+                        return (tokenlist_t) {NULL, 0};
+                    }
+                }
+
+                token->argc = argc;
+            }
 
             #ifdef DEBUG
             printf("Made token_t %I64u: { [%s], %I64u }\n", tokenList.count,
-                tok->value,
-                tok->hash);
+                token->value,
+                token->hash);
             #endif
 
-            tokenList.tokens[tokenList.count] = tok;
+            tokenList.tokens[tokenList.count] = token;
             tokenList.count += 1;
 
             /* Make room for new tokens if the list is full */
@@ -50,6 +67,8 @@ tokenlist_t Tokenize(char* str) {
                 tokenList.tokens = tmp;
             }
         }
+
+        /* Increment by two, so it points to the character after the whitespace */
         startIndex = endIndex + 1;
     }
 
@@ -60,19 +79,50 @@ tokenlist_t Tokenize(char* str) {
 size_t GetNextEndIndex(char* str, size_t start, size_t len) {
 
     for (size_t i = start; i < len + 1; ++i) {
-        
-        switch(str[i]) {
-            case ' ':
-                return i;
-            default:
-                if (i == len) {
-                    return i;
-                }
+
+        if (isspace(str[i])) {
+            return i;
+        } else if (i == len) {
+            return i;
         }
 
     }
 
     return 0;
+}
+
+/* iptr is used to update the endpointer in the main function */
+data_t* GetNextData(char* str, size_t* iptr, size_t len, uint32_t type) {
+
+    /* return NULL if there's no characters left */
+    if (*iptr == len) {
+        return NULL;
+    }
+
+    data_t *data = malloc(sizeof(data_t));
+    data->type = type;
+
+    size_t endIndex;
+
+    /* Keep incrementing *iptr until an endIndex unequal to *iptr is found */
+    do {
+        *iptr += 1;
+        endIndex = GetNextEndIndex(str, *iptr, len);
+    } while (*iptr == endIndex);
+
+    /* Copy the text into a temporary variable */
+    char* tmp = calloc(endIndex - *iptr + 1, sizeof(char));
+    memcpy(tmp, str + *iptr, endIndex - *iptr);
+
+    if (type == DATA_TEXT) {
+        data->text = tmp;
+    } else {
+        data->number = strtol(tmp, NULL, 0);
+    }
+
+    /* update the main index */
+    *iptr = endIndex;
+    return data;
 }
 
 uint64_t GetHashCode(char *str) {
